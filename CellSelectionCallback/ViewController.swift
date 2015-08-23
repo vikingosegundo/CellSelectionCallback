7bf9eaa3b8a7013a4e4e63c89165d9b48a86c143
Manuel Meyer <manuel@vikingosegundo.de>
2015-08-23 20:57:11 +0200

    Initial Commit

---
+++ CellSelectionCallback/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CellSelectionCallback
+//
+//  Created by Manuel Meyer on 23.08.15.
+//  Copyright (c) 2015 Manuel Meyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
